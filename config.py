3821559cd0e633fe1702375c351c71bde0897270
LIM QI HONG <117577524+K-rono@users.noreply.github.com>
2025-12-09 09:08:29 +0800

    Update database configuration settings

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "employee.cnsgngtwiwrc.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "Bait3273"
-customdb = "employee"
-custombucket = "efye-employee"
+customhost = "employee1.chsokg621znk.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "admin123"
+customdb = "employee1"
+custombucket = "limqihong-employee"
 customregion = "us-east-1"
 
