040d559cee63dbdb7e8107ca1a9c2673e268be0b
LIM QI HONG <117577524+K-rono@users.noreply.github.com>
2025-12-09 09:22:05 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 customhost = "employee1.chsokg621znk.us-east-1.rds.amazonaws.com"
 customuser = "admin"
 custompass = "admin123"
-customdb = "employee1"
+customdb = "employee"
 custombucket = "limqihong-employee"
 customregion = "us-east-1"
 
